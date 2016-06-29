20dc2a7e80bd30d5fa64e08523515457b848c47f
Patricia Lakatosova <Patricia Lakatosova>
2016-06-29 22:32:29 +0200

    mapa+prekreslovanie

--- Bomberman/Bomberman/Form1.Designer.cs
+++ Bomberman/Bomberman/Form1.Designer.cs
@@ -29,11 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(313, 89);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Nova hra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(784, 661);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Bomberman/Bomberman/Form1.cs
+++ Bomberman/Bomberman/Form1.cs
@@ -14,7 +14,99 @@ namespace Bomberman
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();           
+        }
+        Graphics g;
+        Mapa m;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            g = CreateGraphics();
+            m = new Mapa("mapa1.txt", "ikonky.bmp", g);
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            m.Prekresli(g);
+        }
+    }
+
+    public class Mapa
+    {
+        private char[,] mapa;
+        int sirka;          //hracieho pola
+        int vyska;
+        int sx;         //sirka stvorceka v pixeloch
+        Bitmap[] ikonky;
+
+        public Mapa(String cestaMapa,String cestaIkonky, Graphics g)
+        {
+            NacitajIkonky(cestaIkonky);
+            NacitajMapu(cestaMapa);            
+        }
+
+        public void NacitajMapu(String cesta)
+        {
+            System.IO.StreamReader sr = new System.IO.StreamReader(cesta);
+            sirka = int.Parse(sr.ReadLine());
+            vyska = int.Parse(sr.ReadLine());
+            mapa = new char[vyska,sirka];
+
+            for (int i = 0; i < vyska; i++)
+            {
+                String riadok = sr.ReadLine();
+                for (int j = 0; j < sirka; j++)
+                {                   
+                    mapa[i,j] = riadok[j];
+                }
+            }
+            sr.Close();
+        }
+
+        public void NacitajIkonky(String cesta)
+        {
+            Bitmap bmp = new Bitmap(cesta);
+            this.sx = bmp.Height;
+            int pocet = bmp.Width / sx;
+            ikonky = new Bitmap[pocet];
+
+            for (int i = 0; i < pocet; i++)
+            {
+                Rectangle rect = new Rectangle(sx * i, 0, sx, sx);
+                ikonky[i] = bmp.Clone(rect, System.Drawing.Imaging.PixelFormat.DontCare);
+            }
+        }
+
+        public void Prekresli(Graphics g)
+        {
+            for (int y = 0; y < vyska; y++)
+            {
+                for (int x = 0; x < sirka; x++)
+                {
+                    char c = mapa[y,x];
+                    int index = "PnKH".IndexOf(c) % 3;
+                    g.DrawImage(ikonky[index],sx*x,sx*y);
+                }
+            }
+        }
+    }
+
+    public class Postavicka
+    {
+        public int px;
+        public int py;
+        public int radius;
+    }
+
+    public class Bman:Postavicka
+    {
+        public Bman(int x,int y,int r)
+        {
+            px = x;
+            py = y;
+            radius = r;
         }
     }
 }
